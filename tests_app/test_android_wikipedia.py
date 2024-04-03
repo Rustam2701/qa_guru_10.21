5583a18fbc149b29644119a06a3dd45dc388dd8a
Rustam <kodzokoff2016@yandex.ru>
2024-04-03 12:30:09 +0300

    add new def

--- tests_app/test_android_wikipedia.py
+++ tests_app/test_android_wikipedia.py
@@ -12,14 +12,3 @@ def test_search():
         results = browser.all((AppiumBy.ID, 'org.wikipedia.alpha:id/page_list_item_title'))
         results.should(have.size_greater_than(0))
         results.first.should(have.text('Appium'))
-
-
-def test_open_article():
-    with step('Type search article "Kitimat"'):
-        browser.element((AppiumBy.ACCESSIBILITY_ID, "Search Wikipedia")).click()
-        browser.element((AppiumBy.ID, "org.wikipedia.alpha:id/search_src_text")).type('Kitimat')
-
-    with step('Verify content found'):
-        results = browser.all((AppiumBy.ID, 'org.wikipedia.alpha:id/page_list_item_title'))
-        results.should(have.size_greater_than(0))
-        results.first.should(have.text('Appium'))
